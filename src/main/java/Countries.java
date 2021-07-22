d81baae7e74c8fcbb0331d19ebfb2880de752635
richardlara35 <richard.u.lara35@gmail.com>
2021-07-22 09:32:49 -0500

    initial

---
+++ src/main/java/Countries.java
@@ -0,0 +1,7 @@
+public class Countries {
+
+    //Create a new Maven project (remember it goes in its own repo and directly under IdeaProjects). Create a `Countries`
+    // class and add at least 3 fields to it and then create an instance of that class and use at least 2 of the fields
+    // in that instance within the main method.
+
+}
